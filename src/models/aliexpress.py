4d9f4f81d107c3884d0ad2ed81ed4f84529e8dc5
Jeferson <mx,jeferson.12@gmail.com>
2021-08-26 11:08:57 -0300

    separate price and filter amazon link

--- src/models/aliexpress.py
+++ src/models/aliexpress.py
@@ -41,13 +41,14 @@ def crawl_aliexpress(url, root_path, nameOfFile):
             driver.execute_script("arguments[0].click();", WebDriverWait(navlist[location], 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "span.tab-inner-text"))))
 
     driver = setSelenium(root_path, False)
-    driver.execute_cdp_cmd("Page.setGeolocationOverride", params = {"latitude": -23.5660791, "longitude": -46.652984, "accuracy": 100 })
+    driver.execute_cdp_cmd("Page.setGeolocationOverride", {"latitude": -23.5660791, "longitude": -46.652984, "accuracy": 100 })
     driver.get(url)
     try:
         print('> iniciando...')
         # select_region(driver)
-
-
+        lang = driver.execute_script("return window.navigator.userlanguage || window.navigator.language")
+        print('Lingua: ',lang)
+        
         smooth_scroll(driver) 
         # smooth_scroll(driver)   
         print('> selecionando ficha tecníca...')
