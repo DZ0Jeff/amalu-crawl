a5595e1e41198a0b43cfc48a8424461a4e1d13cd
Jeff <mx.jeferson.12@gmail.com>
2022-01-07 15:21:33 -0300

    debug: find  crashing error on update

--- utils/parser_handler/__init__.py
+++ utils/parser_handler/__init__.py
@@ -6,7 +6,7 @@ from requests.exceptions import InvalidSchema
 def init_crawler(url):
     try:
         headers = {'User-agent': "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
-        page = requests.get(url, headers=headers)
+        page = requests.get(url, headers=headers, timeout=10)
 
         if page.status_code != 200:
             print(f'[ERRO {page.status_code}] Site indisponivel, tente novamente mais tarde')
